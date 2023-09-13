396137f96877093674920a8cc46b67086057ad75
Ruben-Armas <rubenarmas2010@gmail.com>
2023-09-13 14:02:39 +0100

    IUMA Cards Legacy Style CSS Modificado ligeramente

--- wp-content/themes/iuma_ulpgc/functions.php
+++ wp-content/themes/iuma_ulpgc/functions.php
@@ -91,7 +91,7 @@ function cargar_editor_assets() {
 add_action( 'enqueue_block_editor_assets', 'cargar_editor_assets' );
 
 
-//iuma Cards/tarjetas style
+//IUMA Cards/tarjetas style Legacy
 function agregar_estilos_tarjetas_iuma_legacy() {
     wp_enqueue_style('estilos-tarjetas_iuma', get_template_directory_uri() . '/css/card_iuma_legacy.css');
 }
