9683d86cf11d6662754e7328a00c550880280c26
Ruben-Armas <rubenarmas2010@gmail.com>
2023-12-14 14:15:51 +0000

    Changing Single_news Style

--- wp-content/themes/iuma_ulpgc/functions-elements/style/scss/single_news.php
+++ wp-content/themes/iuma_ulpgc/functions-elements/style/scss/single_news.php
@@ -3,7 +3,7 @@
 function agregar_estilo_noticias_single() {
   wp_enqueue_style(
     'single_noticia',
-    get_template_directory_uri() . '/css/custom_styles/custom_single_noticia.scss');
+    get_template_directory_uri() . '/css/custom_styles/custom_single_noticia.css');
 }
 add_action('wp_enqueue_scripts', 'agregar_estilo_noticias_single');
 
